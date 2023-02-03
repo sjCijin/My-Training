3245ea78b2d540fc4aa6ce718774c68986c5dc98
CIJIN SJ <122510080+sjCijin@users.noreply.github.com>
2023-02-03 15:53:34 +0530

    abstraction examples

---
+++ My_Maven/src/main/java/abstraction/AbsExample.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+public abstract class AbsExample {
+	public abstract void print();
+	public void method1()
+	{
+	 int a=34;
+	System.out.println(a);
+
+}
+}
---
+++ My_Maven/src/main/java/abstraction/AbsExample1.java
@@ -0,0 +1,17 @@
+package abstraction;
+
+public  class AbsExample1 extends AbsExample {
+	public void print()
+	{
+		System.out.println("Hai");
+		
+	}
+	public static void main(String[] args)
+	{
+		AbsExample1 sc=new AbsExample1();
+		sc.method1();
+		sc.print();
+	}
+	
+
+}
